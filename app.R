b6d59933be8ae6fcbd113ee77cef059144e1f7b6
hannespfeiffer <hannes.pfeiffer@gmx.at>
2018-06-07 17:30:43 +0200

    - UI update - marking points in both graphs simultaneously

--- app.R
+++ app.R
@@ -5,12 +5,12 @@ ui <- fluidPage(
   
   headerPanel("Imputation Visualizer"),
   sidebarPanel(
-    textOutput("packages"),
-    selectInput('xcol', 'X Variable', {.packages()}),
-    selectInput('ycol', 'Y Variable', names(iris),
-      selected = names(iris)[[2]]),
-    numericInput('clusters', 'Imputation method', 3,
-      min = 1, max = 9)
+    textOutput("Imputation packages"),
+    checkboxInput("method1","Method 1",FALSE),
+    checkboxInput("method2","Method 2",FALSE),
+    checkboxInput("method3","Method 3",FALSE),
+    selectInput('display_method', 'Missing value display method', 
+                c("Error Boundaries","Boxplots"))
   ),
   mainPanel(
     plotOutput('plot1', click="plot1_click", brush="plot1_brush"),
@@ -30,78 +30,132 @@ server <- function(input, output) {
   sunvalue = sunspot$value[wholeyear_mask]
   cycle = c(seq(4,11), rep(seq(1,11),23), seq(1:4))
   factor_cycle = factor(cycle)
-  vals <- reactiveValues(data=data.frame(year,sunvalue,factor_cycle))
+  sun_df = data=data.frame(year,sunvalue,factor_cycle)
+  points = data.frame(year = numeric(),
+                      sunvalue=numeric(),
+                      factor_cycle=factor())
+  vals <- reactiveValues(markedPoints = points)
   
-  #selectedData <- reactive({
-  #  iris[, c(input$xcol, input$ycol)]
-  #})
-
-  #clusters <- reactive({
-  #  kmeans(selectedData(), input$clusters)
-  #})
+  #plot1
+  plot1 <- ggplot(sun_df,aes(x=year,y=sunvalue)) +
+    geom_line(color="blue", size=0.4) +
+    geom_point(fill="white", shape=21) +
+    labs(x = "Year", y = "Sunvalue") +
+    ggtitle("Sunspot cycles\n\nLinear graph") +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.spacing = unit(0, "lines"),
+          plot.title = element_text(size = rel(1.5), face = "bold", vjust = 1.5),
+          #axis.text.x = element_blank(),
+          #axis.ticks.x = element_blank(),
+          strip.background = element_rect(fill = "white")) 
+  
+  #plot2
+  plot2 <- ggplot(sun_df, aes(x = year, y = sunvalue)) +
+    geom_line(color="darkseagreen", size=0.4) +
+    geom_point(fill="white", shape=21) +
+    stat_smooth(method = "lm", formula = y ~ 1, se = FALSE, colour = "red", size=0.2) +
+    facet_wrap(~ factor_cycle, nrow = 1) +
+    labs(x = NULL, y = "Sunvalue") +
+    ggtitle("Cyclic graph") +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.spacing = unit(0, "lines"),
+          #axis.line = element_line(color="green"),
+          plot.title = element_text(size = rel(1.5), face = "bold", vjust = 1.5),
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank(),
+          strip.background = element_rect(fill = "white")) 
   
   observeEvent(input$plot1_click,{
-    nearpoint <- nearPoints(vals$data,input$plot1_click, 
-                            xvar = "year", yvar="sunvalue",
-                            threshold = 5, maxpoints = 1)
-    #if (nrow(nearpoint) != 0){
-    #  vals$data <- rbind(vals$data,
-    #    data.frame(year=nearpoint$year,value=nearpoint$value,fcycle=13))
-    #}
+    nearpoint <- nearPoints(sun_df,input$plot1_click, 
+                  xvar = "year", yvar="sunvalue",
+                  threshold = 5, maxpoints = 1)
     if (nrow(nearpoint) != 0){
-    vals$data <- rbind(vals$data, data.frame(
+      vals$markedPoints <- vals$markedPoints[0,]
+      vals$markedPoints <- rbind(vals$points, data.frame(
       year=nearpoint$year,
       sunvalue=nearpoint$sunvalue,
-      factor_cycle=as.factor(13)))
+     factor_cycle=nearpoint$factor_cycle))
+      #vals$data$year[vals$data$year %in% c(nearpoint$year)]=NA
     }
-    
+  })
+  observeEvent(input$plot2_click,{
+    nearpoint <- nearPoints(sun_df,input$plot2_click, 
+                            xvar = "year", yvar="sunvalue",
+                            threshold = 5, maxpoints = 1)
+    if (nrow(nearpoint) != 0){
+      vals$markedPoints <- vals$markedPoints[0,]
+      vals$markedPoints <- rbind(vals$points, data.frame(
+        year=nearpoint$year,
+        sunvalue=nearpoint$sunvalue,
+        factor_cycle=nearpoint$factor_cycle))
+      #vals$data$year[vals$data$year %in% c(nearpoint$year)]=NA
+    }
   })
 
   output$info <- renderPrint({
-    row <- nearPoints(vals$data,input$plot1_click, 
+    row <- nearPoints(sun_df,input$plot1_click, 
                       xvar = "year", yvar="sunvalue",
                       threshold = 5, maxpoints = 1)
-    row2 <- nearPoints(vals$data,input$plot2_click,
+    row2 <- nearPoints(sun_df,input$plot2_click,
                        xvar = "year", yvar = "sunvalue",
                        threshold = 5, maxpoints = 1)
     print(row)
     print(row2)
-    print(vals$data)
+    #print(vals$data)
   })
   
   output$plot1 <- renderPlot({
-    ggplot(vals$data,aes(x=year,y=sunvalue)) +
-      geom_line(color="blue", size=0.4) +
-      geom_point(fill="white", shape=21) +
-      labs(x = "Year", y = "Sunvalue") +
-      ggtitle("Sunspot cycles\n\nLinear graph") +
-      theme_bw() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            panel.spacing = unit(0, "lines"),
-            plot.title = element_text(size = rel(1.5), face = "bold", vjust = 1.5),
-            #axis.text.x = element_blank(),
-            #axis.ticks.x = element_blank(),
-            strip.background = element_rect(fill = "white")) 
+    plot1 + geom_point(data=vals$markedPoints, color="red", size=2)
+    #plot1 + geom_point(data= nearPoints(sun_df,input$plot1_click, 
+    #                                    xvar = "year", yvar="sunvalue",
+    #                                    threshold = 5, maxpoints = 1),
+    #                   color="red", size=2)
+    
+    
+    #ggplot(vals$data,aes(x=year,y=sunvalue)) +
+      #geom_line(color="blue", size=0.4) +
+      #geom_point(fill="white", shape=21) +
+      #geom_point(data=vals$markedPoints, color="red", size=2) +
+      #labs(x = "Year", y = "Sunvalue") +
+      #ggtitle("Sunspot cycles\n\nLinear graph") +
+     #theme_bw() +
+      #theme(panel.grid.major = element_blank(),
+      #      panel.grid.minor = element_blank(),
+      #      panel.spacing = unit(0, "lines"),
+      #      plot.title = element_text(size = rel(1.5), face = "bold", vjust = 1.5),
+      #      #axis.text.x = element_blank(),
+      #      #axis.ticks.x = element_blank(),
+      #      strip.background = element_rect(fill = "white")) 
   })
   
   output$plot2 <- renderPlot({
-    ggplot(vals$data, aes(x = year, y = sunvalue)) +
-      geom_line(color="darkseagreen", size=0.4) +
-      geom_point(fill="white", shape=21) +
-      stat_smooth(method = "lm", formula = y ~ 1, se = FALSE, colour = "red", size=0.2) +
-      facet_wrap(~ factor_cycle, nrow = 1) +
-      labs(x = NULL, y = "Sunvalue") +
-      ggtitle("Cyclic graph") +
-      theme_bw() +
-      theme(panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            panel.spacing = unit(0, "lines"),
-            #axis.line = element_line(color="green"),
-            plot.title = element_text(size = rel(1.5), face = "bold", vjust = 1.5),
-            axis.text.x = element_blank(),
-            axis.ticks.x = element_blank(),
-            strip.background = element_rect(fill = "white")) 
+    plot2 + geom_point(data=vals$markedPoints, color="red", size=2)
+    #plot2 + geom_point(data= nearPoints(sun_df,input$plot1_click, 
+    #                                    xvar = "year", yvar="sunvalue",
+    #                                    threshold = 5, maxpoints = 1),
+    #                   color="red", size=2)
+    
+    #ggplot(vals$data, aes(x = year, y = sunvalue)) +
+    #  geom_line(color="darkseagreen", size=0.4) +
+    #  geom_point(fill="white", shape=21) +
+    #  geom_point(data=vals$markedPoints, color="red", size=2) +
+    #  stat_smooth(method = "lm", formula = y ~ 1, se = FALSE, colour = "red", size=0.2) +
+    #  facet_wrap(~ factor_cycle, nrow = 1) +
+    #  labs(x = NULL, y = "Sunvalue") +
+    #  ggtitle("Cyclic graph") +
+     # theme_bw() +
+    #  theme(panel.grid.major = element_blank(),
+    #        panel.grid.minor = element_blank(),
+    #        panel.spacing = unit(0, "lines"),
+    #        #axis.line = element_line(color="green"),
+    #        plot.title = element_text(size = rel(1.5), face = "bold", vjust = 1.5),
+    #        axis.text.x = element_blank(),
+    #        axis.ticks.x = element_blank(),
+    #        strip.background = element_rect(fill = "white")) 
   }) 
 }
 
